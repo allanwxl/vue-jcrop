eec5c3b3c0ce63aef1a7ec2c54babca76e37b46b
wangxuanlv@jiaomatech.com <wangxuanlv@jiaomatech.com>
2019-05-27 10:42:23 +0800

    增加多个rect初始化 react DOM节点增加id 规则是rect+index

--- src/components/Jcrop.vue
+++ src/components/Jcrop.vue
@@ -1,13 +1,13 @@
 <template>
-  <div v-once/>
+  <div v-once></div>
 </template>
 
 <script>
-import Jcrop from 'jcrop';
+import Jcrop from '../jcrop';
 
 export default {
   name: 'Jcrop',
-  props: [ 'src', 'options', 'rect' ],
+  props: [ 'src', 'options', 'rect', 'initRects' ],
   mounted: function() {
     const img = new Image();
     this.$el.appendChild(img);
@@ -18,21 +18,38 @@ export default {
     startup(img){
       console.log(this.options);
       this.jcrop = Jcrop.attach(img,this.options||{});
-      var rect = Jcrop.Rect.sizeOf(this.jcrop.el);
+      let rect = Jcrop.Rect.sizeOf(this.jcrop.el);
 
       ['activate','update','change','remove'].forEach(ev => {
         this.jcrop.listen('crop.'+ev,widget => this.$emit(ev,widget));
       });
 
-      if (this.rect) rect = Jcrop.Rect.from(this.rect);
-      else rect = rect.scale(.7,.5).center(rect.w,rect.h)
-
-      this.jcrop.newWidget(rect);
+      if (this.rect) {
+        rect = Jcrop.Rect.from(this.rect);
+        this.jcrop.newWidget(rect);
+      }
+      // else rect = rect.scale(.7,.5).center(rect.w,rect.h)
+      if (this.initRects && this.initRects.length > 0) {
+        this.initRects.forEach( el => {
+          rect = Jcrop.Rect.from(el);
+          this.jcrop.newWidget(rect);
+        })
+      }
       this.pos = this.jcrop.pos;
       this.jcrop.focus();
     }
   },
   watch: {
+    initRects: {
+      handler: function(v) {
+        this.jcrop.destroy();
+        const img = new Image();
+        this.$el.appendChild(img);
+        img.src = this.src;
+        Jcrop.load(img).then(this.startup);
+      },
+      deep: true
+    },
     rect: {
       handler: function(v) {
         if (!this.jcrop.active) return false;
@@ -59,5 +76,5 @@ export default {
 </script>
 
 <style lang="scss">
-@import "~jcrop/build/css/jcrop.scss";
+@import "../Jcrop/build/css/jcrop.scss";
 </style>
---
+++ src/jcrop/build/js/handle.js
@@ -0,0 +1,13 @@
+
+import DomObj from './domobj';
+
+class Handle extends DomObj {
+}
+
+Handle.create = function (clname) {
+  const el = document.createElement('div');
+  el.className = clname;
+  return new Handle(el);
+};
+
+export default Handle;
